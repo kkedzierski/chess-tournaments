5a22be4fc838c9f8de3518e93bd23f38b3fcf27e
kkedzierski <k.kedzierski@global4net.com>
2024-06-26 17:50:40 +0200

    #main Documentation update

--- documentation/deploy_docs.sh
+++ documentation/deploy_docs.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-echo "Building documentation"
-mkdocs build -f documentation/config/pl/mkdocs.yml
-mkdocs build -f documentation/config/en/mkdocs.yml
+branch_name=$(git symbolic-ref --short -q HEAD)
 
-echo "Deploying documentation"
-mkdocs gh-deploy -f documentation/config/pl/mkdocs.yml
-mkdocs gh-deploy -f documentation/config/en/mkdocs.yml
+echo "Pushing changes to the repository"
+echo "Current branch: $branch_name";
+if [ ${branch_name} == 'main' ]; then
+    git add documentation/
+    git commit -m "#main Documentation update"
+    git push origin main
+else
+  echo "skipping documentation update for branch $branch_name";
+fi
+
+
+#echo "Building documentation"
+#mkdocs build -f documentation/config/pl/mkdocs.yml
+#mkdocs build -f documentation/config/en/mkdocs.yml
+#
+#echo "Deploying documentation"
+#mkdocs gh-deploy -f documentation/config/pl/mkdocs.yml
+#mkdocs gh-deploy -f documentation/config/en/mkdocs.yml
