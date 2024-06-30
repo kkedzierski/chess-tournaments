f7eb87ee27881265107f871335261c253b568a7f
kkedzierski <k.kedzierski@global4net.com>
2024-06-30 11:31:58 +0200

    #main Documentation update

--- documentation/deploy_docs.sh
+++ documentation/deploy_docs.sh
@@ -20,15 +20,15 @@ else
         git add documentation/
 
         git commit -m "#main Documentation update"
-#        git push origin main
+        git push origin main
     else
       echo "Skipping pushing documentation to repository for branch $branch_name.";
     fi
-
+#
 #    echo "Building documentation..."
 #    mkdocs build -f documentation/config/pl/mkdocs.yml
 #    mkdocs build -f documentation/config/en/mkdocs.yml
-
+#
 #    echo "Deploying documentation..."
 #    mkdocs gh-deploy -f documentation/config/pl/mkdocs.yml
 #    mkdocs gh-deploy -f documentation/config/en/mkdocs.yml
